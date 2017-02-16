7dd2d30f47b17860e1aff70ddfe840915e7a5015
Siddharth <siddharthgupta3391@gmail.com>
2017-02-16 17:20:46 -0600

    initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    consumer_key:         'J2zk5aXyxNPF4ZxTulj6AW2GC',
+    consumer_secret:      'Nb7mGOuvj6vVYyerxsAaLV902uZmFXbY5e8TSggO5xHNesAIjJ',
+    access_token:         '831359129637777408-DvF9tNC5fivjJHOWk92B2K4umMsHalm',
+    access_token_secret:  'h5RsvxOlxiUXu4iDoJ1sVBeZlAkNwdPwkT9M3mehassGZ',
+
+}
